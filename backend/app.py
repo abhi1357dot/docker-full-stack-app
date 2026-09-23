f54e456aff2697803ba97b44081435b1a31ff967
abhi1357dot <acharyamohan016@gmail.com>
2026-09-23 17:18:25 +0530

    Added HTML in backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 
 app = Flask(__name__)
 
@@ -29,7 +29,7 @@ def submit():
 
 @app.route("/")
 def home():
-    return "Flask Backend is running!"
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
